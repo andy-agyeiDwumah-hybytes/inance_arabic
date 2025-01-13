fd59a0bbb6e571a74f039dc5eeb5b041e5f19767
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-01-13 23:44:46 +0000

    Improve quality of comments in useChangeLanguage hook

--- src/hooks/useLanguageChange.js
+++ src/hooks/useLanguageChange.js
@@ -4,9 +4,7 @@ import { useNavigate, useNavigationType } from "react-router";
 // Languages
 import { languages } from "../languages/languageCodes";
 // Constants
-import {
-  LOCALSTORAGEKEY,
-} from "../constants/Constants";
+import { LOCALSTORAGEKEY } from "../constants/Constants";
 // Context
 import { LanguageContext } from "../context/languageContext";
 // i18
@@ -16,11 +14,11 @@ export default function useLanguageChange(langCode, pathname, currentPage) {
   const { languageOptions, setLanguageOptions } = useContext(LanguageContext);
   const { i18n } = useTranslation();
   const navigate = useNavigate();
-  const navigationType = useNavigationType()
+  const navigationType = useNavigationType();
 
   // Update direction when current language direction does not match
   if (document.documentElement.style.direction !== i18n.dir()) {
-    document.documentElement.setAttribute("dir", i18n.dir())
+    document.documentElement.setAttribute("dir", i18n.dir());
   }
 
   useEffect(() => {
@@ -35,38 +33,61 @@ export default function useLanguageChange(langCode, pathname, currentPage) {
 
     // langCode === "home" - when pathname is "/"
     if (languageCodeExists || langCode === "home") {
-      // Page refresh or back/forward navigation detected
       if (navigationType === "POP") {
-        console.log("update page: custom hook");
+        console.log(
+          "Page refresh or back/forward navigation detected. " +
+          "Updating page (custom hook)."
+        );
         if (langCode === "home") {
-          console.log("No lang code provided. navigating to language home page");
+          console.log(
+            "No language code provided. Navigate to " +
+            "language home page (custom hook)."
+          );
           // Value comes from local storage (see App.jsx)
           navigate(`/${languageOptions.language}`);
           i18n.changeLanguage(languageOptions.language);
-        }
-        else if (languageOptions.language !== langCode) {
-          console.log("Local storage key not equal to lang code")
-          // Update local storage and language state
-          const updatedOptions = { language: langCode, textDirection: languageCodeExists.writingMode }
-          localStorage.setItem(LOCALSTORAGEKEY, JSON.stringify(updatedOptions))
+        } else if (languageOptions.language !== langCode) {
+          console.log(
+            "Local storage key not equal to language code. Save " +
+            "language code to local storage. Update state. Change " +
+            "language (custom hook)."
+          );
+          const updatedOptions = {
+            language: langCode,
+            textDirection: languageCodeExists.writingMode,
+          };
+          localStorage.setItem(LOCALSTORAGEKEY, JSON.stringify(updatedOptions));
           setLanguageOptions({
             language: languageCodeExists.languageCode,
             textDirection: languageCodeExists.writingMode,
           });
           i18n.changeLanguage(langCode);
         } else {
-          console.log("Local storage key equal to lang code.");
+          console.log(
+            "Local storage key equal to language code. " +
+            "Change language (custom hook)."
+          );
           i18n.changeLanguage(languageCodeExists.languageCode);
         }
       } else {
-        // When a link is clicked - do not update state, local storage, or change language
-        console.log("no update: custom hook");
+        console.log("Link was clicked. No update (custom hook).");
       }
     } else {
-      // Language code does not exist - navigate back to language home page found in local storage
       navigate(`/${languageOptions.language}`);
       i18n.changeLanguage(languageOptions.language);
-      console.log("language code doesn't exist")
+      console.log(
+        "Language code doesn't exist. Navigate to language home " +
+        "page found in local storage. Change language(custom hook)."
+      );
     }
-  }, [i18n, langCode, pathname, currentPage, navigate, navigationType, setLanguageOptions, languageOptions])
+  }, [
+    i18n,
+    langCode,
+    pathname,
+    currentPage,
+    navigate,
+    navigationType,
+    setLanguageOptions,
+    languageOptions,
+  ]);
 }
